a7440a9eedb2ea6c7e0448807474a58c5baaf0e4
philothoph <philothoph@gmail.com>
2024-05-10 15:35:25 +0400

    hide tooltips after 2 seconds

--- static/tooltip.js
+++ static/tooltip.js
@@ -1,11 +1,21 @@
+// Variable for delay in milliseconds
+const DELAY = 2000;
+
 // Function to enable tooltips
 /**
  * Enables tooltips by selecting all elements with 'data-bs-toggle="tooltip"' attribute
  * and initializing Bootstrap tooltips for each element.
+ * Attaches a event listener to each tooltip element to hide after 2 seconds.
  */
 function enableTooltips() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => {
+        const tooltipInstance = new bootstrap.Tooltip(tooltipTriggerEl);
+        tooltipTriggerEl.addEventListener('show.bs.tooltip', () => {
+            setTimeout(() => tooltipInstance.hide(), DELAY);             
+        });
+        return tooltipInstance;
+    });
 }
 
 
